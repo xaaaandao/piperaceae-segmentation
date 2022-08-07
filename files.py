75cf708e346b256089da490885a53d611b75bd14
Alexandre Kajihara <alexandre.ykz@gmail.com>
2022-08-06 21:51:45 -0300

    update save figs

--- files.py
+++ files.py
@@ -14,19 +14,18 @@ def create_folder(list_path):
         pathlib.Path(path).mkdir(parents=True, exist_ok=True)
 
 
-def save_figs(list_images_names, model, path, x):
-    x_pred = model.predict(x)
-    for idx in range(x_pred.shape[0]):
-        print(f"save img {list_images_names[idx]}")
-        pred_mask = numpy.uint8(x_pred[idx] >= 0.5)
-        filename_pred_mask = list_images_names[idx] + "mask_unet.png"
-        skimage.io.imsave(os.path.join(path, filename_pred_mask), skimage.img_as_ubyte(pred_mask * 255))
+def save_figs(cfg, list_images_names, list_index, model, path, x):
+    for index in list_index:
+        image = x[index].reshape((1, cfg["image_size"], cfg["image_size"], cfg["channel"]))
+        x_pred = model.predict(image)
+        pred_mask = numpy.uint8(x_pred >= 0.5)
+        filename_pred_mask = list_images_names[index] + "mask_unet.png"
+        skimage.io.imsave(os.path.join(path, filename_pred_mask), skimage.img_as_ubyte(pred_mask[0, :, :, 0] * 255))
 
-        image = x[idx]
         image_pred_mask = image * pred_mask
         image_pred_mask[image_pred_mask == 0] = 1
-        filename_image_pred_mask = list_images_names[idx] + "w_pred_mask.png"
-        skimage.io.imsave(os.path.join(path, filename_image_pred_mask), skimage.img_as_ubyte(image_pred_mask))
+        filename_image_pred_mask = list_images_names[index] + "w_pred_mask.png"
+        skimage.io.imsave(os.path.join(path, filename_image_pred_mask), skimage.img_as_ubyte(image_pred_mask[0, :, :, 0]))
 
 
 def save_fit_history(fold, fit, path):
