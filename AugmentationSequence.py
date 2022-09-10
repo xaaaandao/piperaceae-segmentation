90a4c2316e86e8050bc911b27970de923f072e1f
Alexandre Yuji Kajihara <alexandre.ykz@gmail.com>
2022-09-10 16:35:13 -0300

    Somente treinamento

--- AugmentationSequence.py
+++ AugmentationSequence.py
@@ -2,16 +2,49 @@ import numpy
 import tensorflow
 
 
+class CreateSequence(tensorflow.keras.utils.Sequence):
+    """Helper to iterate over the data (as Numpy arrays)."""
+
+    def __init__(self, batch_size, img_size, input_img_paths, target_img_paths):
+        self.batch_size = batch_size
+        self.img_size = img_size
+        self.input_img_paths = input_img_paths
+        self.target_img_paths = target_img_paths
+
+    def __len__(self):
+        return len(self.target_img_paths) // self.batch_size
+
+    def __getitem__(self, idx):
+        """Returns tuple (input, target) correspond to batch #idx."""
+        i = idx * self.batch_size
+        batch_input_img_paths = self.input_img_paths[i : i + self.batch_size]
+        batch_target_img_paths = self.target_img_paths[i : i + self.batch_size]
+        x = numpy.zeros((self.batch_size,) + self.img_size + (3,), dtype="float32")
+        for j, path in enumerate(batch_input_img_paths):
+            img = tensorflow.keras.preprocessing.image.load_img(path, target_size=self.img_size)
+            x[j] = img
+        y = numpy.zeros((self.batch_size,) + self.img_size + (1,), dtype="float32")
+        for j, path in enumerate(batch_target_img_paths):
+            img = tensorflow.keras.preprocessing.image.load_img(path, target_size=self.img_size, color_mode="grayscale")
+            y[j] = numpy.expand_dims(img, 2)
+            # Ground truth labels are 1, 2, 3. Subtract one to make them 0, 1, 2:
+            y[j] -= 1
+        return x, y
+
 class AugmentationSequence(tensorflow.keras.utils.Sequence):
     def __init__(self, x_set, y_set, batch_size, augmentations):
         self.x, self.y = x_set, y_set
         self.batch_size = batch_size
         self.augment = augmentations
+        print(f"b{self.x}")
 
     def __len__(self):
+        print(f"b{int(numpy.ceil(len(self.x) / float(self.batch_size)))}")
         return int(numpy.ceil(len(self.x) / float(self.batch_size)))
 
     def __getitem__(self, idx):
+        print(f"c{idx}")
+
         batch_x = self.x[idx * self.batch_size:(idx + 1) * self.batch_size]
         batch_y = self.y[idx * self.batch_size:(idx + 1) * self.batch_size]
 
