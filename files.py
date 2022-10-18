3214c2655fb8d4fa3cfd70752662d1e7cd9dd049
Alexandre Yuji Kajihara <alexandre.ykz@gmail.com>
2022-10-18 20:49:07 -0300

    Up

--- files.py
+++ files.py
@@ -4,6 +4,7 @@ import os
 import pathlib
 import pickle
 import skimage
+import tensorflow as tf
 
 
 def create_folder(list_path):
@@ -14,24 +15,36 @@ def create_folder(list_path):
 def save_figs(cfg, list_images_names, list_index, model, path, x):
     for p in ['mask_unet', 'w_pred_mask']:
         pathlib.Path(os.path.join(path, p)).mkdir(parents=True, exist_ok=True)
+
     for i, index in enumerate(list_index):
-        if cfg['channel'] == 3:
-            save_image_rgb(cfg, list_images_names, x[index], index, model, path)
-        else:
-            save_image_rgb(cfg, list_images_names, x[index], index, model, path)
+        filename = list_images_names[index]
+        # image = x[index] / 255
+        image = x[index].reshape((1, cfg['image_size'], cfg['image_size'], cfg['channel']))
+        mask = model.predict(image)
+        mask = mask[0, :, :, :]
+        new_filename = os.path.join(path, 'mask_unet', filename + '.bmp')
+        print(new_filename, mask.shape)
+        tf.keras.preprocessing.image.save_img(new_filename, mask)
+
+        mask = np.uint8(mask >= 0.5)
+        image_segmented = image * mask
+        image_segmented[image_segmented == 0] = 1
+        image_segmented = image_segmented[0, :, :, :]
+        image_segmented = tf.keras.preprocessing.image.array_to_img(image_segmented)
+        new_filename = os.path.join(path, 'w_pred_mask', filename + '.png')
+        tf.keras.preprocessing.image.save_img(new_filename, image_segmented)
+    #     if cfg['channel'] == 3:
+    #         save_image_rgb(cfg, list_images_names, x[index], index, model, path)
+    #     else:
+    #         save_image_rgb(cfg, list_images_names, x[index], index, model, path)
 
 
-def save_image_rgb(cfg, list_images_names, image, index, model, path):
-    x_pred = model.predict(image.reshape((1, cfg['image_size'], cfg['image_size'], cfg['channel'])))
-    pred_mask = np.uint8(x_pred >= 0.5)
-    filename_pred_mask = list_images_names[index] + 'mask_unet.png'
-    skimage.io.imsave(os.path.join(path, 'mask_unet', filename_pred_mask), skimage.img_as_ubyte(pred_mask[0, :, :, 0] * 255))
 
     # print(x[index].shape)
-    image_segmented = image * pred_mask[0, :, :, :]
-    image_segmented[image_segmented == 0] = 1
-    filename_image_pred_mask = list_images_names[index] + 'w_pred_mask.png'
-    skimage.io.imsave(os.path.join(path, 'w_pred_mask', filename_image_pred_mask), skimage.img_as_ubyte(image_segmented))
+    # image_segmented = image * pred_mask[0, :, :, :]
+    # image_segmented[image_segmented == 0] = 1
+    # filename_image_pred_mask = list_images_names[index] + 'w_pred_mask.png'
+    # skimage.io.imsave(os.path.join(path, 'w_pred_mask', filename_image_pred_mask), skimage.img_as_ubyte(image_segmented))
 
 
 def save_image_grayscale(cfg, list_images_names, image, index, model, path):
