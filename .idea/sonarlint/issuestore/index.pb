209a873a3fd45b998d688ec360014701055bba24
Alexandre Yuji Kajihara <alexandre.ykz@gmail.com>
2022-10-20 20:06:08 -0300

    Up!

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,10 +1,3 @@
 
 8
 image.py,9/d/9dddac0b3405cad06f261d043ad037bc511d6ce0
-:
-
-main.ipynb,b/1/b1094ba51eacb5dd252316aae3bf58b30cc641cb
-8
-files.py,6/5/651a8940746ea8db54b240ab9dd999857a447c84
-=
-predict.ipynb,a/1/a1f0070754b5a48cd68ff1cd0af8116ea8ed06bf
--- files.py
+++ files.py
@@ -1,12 +1,11 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import os
 import pathlib
 import pickle
-import skimage
 import tensorflow as tf
 
 from PIL import Image
+
 
 def create_folder(list_path):
     for path in list_path:
@@ -22,7 +21,6 @@ def save_figs(cfg, list_images_names, list_index, model, path, x):
         mask = model.predict(image)
         mask = mask[0, :, :, :]
         new_filename = os.path.join(path, 'mask_unet', str(list_images_names[index].stem) + '.bmp')
-        print(new_filename, mask.shape)
         tf.keras.preprocessing.image.save_img(new_filename, mask)
 
         mask = tf.keras.preprocessing.image.array_to_img(mask).convert('L')
@@ -37,46 +35,9 @@ def save_figs(cfg, list_images_names, list_index, model, path, x):
         bg_w, bg_h = background.size
         offset = ((bg_w - img_w) // 2, (bg_h - img_h) // 2)
         background.paste(image_original, offset, image_original)
-        new_filename = os.path.join(path, 'w_pred_mask', str(list_images_names[index].stem) + '.png')
+        new_filename = os.path.join(path, 'w_pred_mask', str(list_images_names[index].stem) + '.jpeg')
+        background = background.convert('RGB')
         background.save(new_filename)
-        # filename = str(list_images_names[index].stem)
-        # image = x[index].reshape((1, cfg['image_size'], cfg['image_size'], cfg['channel']))
-        # mask = model.predict(image)
-        # mask = mask[0, :, :, :]
-        # # save mask
-        # new_filename = os.path.join(path, 'mask_unet', filename + '.bmp')
-        # print(new_filename, mask.shape)
-        # tf.keras.preprocessing.image.save_img(new_filename, mask)
-        #
-        # mask = tf.keras.preprocessing.image.array_to_img(mask).convert('L')
-        # # image_original = tf.keras.preprocessing.image.load_img(list_images_names[index].resolve())
-        # image_original = tf.keras.preprocessing.image.array_to_img(x[index])
-        # image_original = image_original.convert('RGBA')
-        # # image_original = tf.keras.preprocessing.image.img_to_array(image_original)
-        # image_original_transparente = image_original.copy()
-        # new_filename = os.path.join(path, 'transparency', filename + '_transparente.png')
-        # image_original_transparente.putalpha(mask)
-        # image_original_transparente.save(new_filename)
-        #
-        # background = Image.new('RGBA', (cfg['image_size'], cfg['image_size']), 'WHITE')
-        # img_w, img_h = image_original.size
-        # bg_w, bg_h = background.size
-        # offset = ((bg_w - img_w) // 2, (bg_h - img_h) // 2)
-        # background.paste(image_original, offset, image_original)
-        # new_filename = os.path.join(path, 'w_pred_mask', filename + '.png')
-        # background.save(new_filename)
-
-def save_image_grayscale(cfg, list_images_names, image, index, model, path):
-    image = image.reshape((1, cfg['image_size'], cfg['image_size'], cfg['channel']))
-    x_pred = model.predict(image)[0, :, :, 0]
-    pred_mask = np.uint8(x_pred >= 0.5)
-    filename_pred_mask = list_images_names[index] + 'mask_unet.png'
-    skimage.io.imsave(os.path.join(path, 'mask_unet', filename_pred_mask), skimage.img_as_ubyte(pred_mask * 255))
-
-    img_segmented = image[0, :, :, 0] * pred_mask
-    img_segmented[img_segmented == 0] = 1
-    filename_image_pred_mask = list_images_names[index] + 'w_pred_mask.png'
-    skimage.io.imsave(os.path.join(path, 'w_pred_mask', filename_image_pred_mask), skimage.img_as_ubyte(img_segmented))
 
 
 def save_fit_history(fold, fit, path):
